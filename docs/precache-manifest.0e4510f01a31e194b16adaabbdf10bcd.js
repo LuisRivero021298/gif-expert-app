1a6435cfe39bb56066055fd4fea492e17b29e5a6
LuisRivero021298 <luisda021298@gmail.com>
2020-07-16 00:46:01 -0500

    docs added

---
+++ docs/precache-manifest.0e4510f01a31e194b16adaabbdf10bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684ddec2c94bc19f9e5c2b54f3b3621e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f2dfea3960c133c496",
+    "url": "/static/css/main.e4a0020b.chunk.css"
+  },
+  {
+    "revision": "d1a1c886bbcb924fc52c",
+    "url": "/static/js/2.e2c23b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2c23b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f2dfea3960c133c496",
+    "url": "/static/js/main.0c9ab087.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
